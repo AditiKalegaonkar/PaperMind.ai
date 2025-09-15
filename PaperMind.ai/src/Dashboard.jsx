95f9e6eb40d73b9886900b57d78ca5c43cdc05e9
AditiKalegaonkar <132119095+AditiKalegaonkar@users.noreply.github.com>
2025-09-15 15:53:03 +0530

    Got the aprila font

--- PaperMind.ai/src/Dashboard.jsx
+++ PaperMind.ai/src/Dashboard.jsx
@@ -3,8 +3,11 @@ import Navbar from "./Navbar.jsx";
 
 function Dashboard() {
   return (
-    <div>
-        <Navbar />
+    <div className="Landing-Page">
+      <Navbar />
+      <section className="Hero quicksand">
+        <h1 className="hero-title">Welcome to PaperMind.ai</h1>
+      </section>
     </div>
   );
 }
