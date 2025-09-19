b6f8c95e120a8dfe559d12d27158c28fe28abe3d
AditiKalegaonkar <132119095+AditiKalegaonkar@users.noreply.github.com>
2025-09-19 11:20:33 +0530

    User (#9)
    * Backend Completed
    
    * Delete temp

---
+++ PaperMind.ai/backend/User.js
@@ -0,0 +1,19 @@
+// User.js (ES Module)
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, trim: true }, // optional
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    phone: {
+      countryCode: { type: String, default: null },
+      number: { type: String, unique: true, sparse: true, default: null }
+    },
+    password: { type: String }, // only for local login
+    googleId: { type: String, default: null },
+    facebookId: { type: String, default: null },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
